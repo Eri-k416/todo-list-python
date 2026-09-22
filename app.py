a13a888763c44762742adacd94517b82a856ef11
Eri_k416 <muhammadfachrydzi@gmail.com>
2026-09-22 17:43:18 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import csv
+import os
+import pandas as pd
+
+FILE_CSV = "task-data.csv"
+
+st.title("To-do app")
+st.text("Aplikasi manajemen tugas ber-deadline",  text_alignment="center")
+
+with st.container(border=True):
+    st.header("Tambah tugas")
+    
+    nama_tugas = st.text_input("Nama tugas :")
+    deadline = st.date_input("Tenggat tanggal tugas :")
+    
+    if st.button("Tambah Tugas ➕"):
+        isFinished = False
+        with open(FILE_CSV, mode="a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([nama_tugas, deadline, isFinished])
+        st.success(f"Tugas \"{nama_tugas}\" sudah di tambahkan.")
+
+with st.container(border=True):
+    st.header("Tugas yang belum selesai")
+    # if os.path.exists(FILE_CSV):
+    #     with open(FILE_CSV) as f:
+    #         df = pd.read_csv(csv.reader(f))
+    #         for row in df.itertuples():
+    #             with st.container(border=True):
+    #                 st.subheader(task)
+    #                 st.text(task[1])
+    #             if st.checkbox():
+with st.container(border=True):
+    st.header("Riwayat tugas yang selesai")
+                    
